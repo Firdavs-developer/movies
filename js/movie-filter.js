744978b6c732d24e1ddd47136ee5babfd48aa198
Firdavs-developer <firdavsdosiyorov3@gmail.com>
2024-01-25 23:10:19 +0500

    I created category movie

--- js/movie-filter.js
+++ js/movie-filter.js
@@ -1,6 +1,7 @@
 let elMovieForm = document.querySelector(".movie-form");
 let elSearchInput = document.querySelector(".movie-search-input");
-
+let elMovieCategory = document.querySelector(".movie-category");
+let elMovieCategoryFragment = new DocumentFragment();
 
 elMovieForm.addEventListener("input", function(){
     let searchInputValue = elSearchInput.value.trim();
@@ -12,3 +13,37 @@ elMovieForm.addEventListener("input", function(){
 
     renderMovies(searchMovies)
 })
+
+const movies_category = [];
+
+movies.forEach((movie) => {
+    movie.Categories.split("|").forEach(category => {
+        if(!movies_category.includes(category)){
+            movies_category.push(category)
+        }
+    })
+});
+
+movies_category.forEach((category) => {
+    let newOption = document.createElement("option");
+    
+    newOption.value = category;
+    newOption.textContent = category;
+    elMovieCategoryFragment.appendChild(newOption)
+})
+
+elMovieCategory.appendChild(elMovieCategoryFragment);
+
+// CATEGORY BO'YICHA FILTER QILADIGAN FUNKSIYA YOZAMIZ
+const filterMoviesByCategory = (category) => {
+    const filterMovies = movies.filter((m) => {
+        m.Categories.split("|").includes(category) || category === "All";
+    })
+    renderMovies(filterMovies)
+}
+
+elMovieCategory.addEventListener("change", () => {
+    let inputValue = elMovieCategory.value;
+
+    filterMoviesByCategory(inputValue)
+} )
